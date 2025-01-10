3acc86f4d47c7f65e2ffbb3f9826114eba344344
sebastian <sdellaera@atlan-tics.com>
2025-01-10 13:16:52 -0300

    Agregando columna rol a tabla usuarios

---
+++ database/migrations/2025_01_10_160935_add_rol_to_usuarios_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('usuarios', function (Blueprint $table) {
+            $table->string('rol')->default('user'); // Por ejemplo, por defecto el rol será 'user'.
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('usuarios', function (Blueprint $table) {
+            //
+        });
+    }
+};
